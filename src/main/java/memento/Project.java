53d851b38b8722566dbea2f45194d1c2490ad9f3
Denis <dendet94@gmail.com>
2023-04-18 11:12:07 +0300

    first commit

--- src/main/java/memento/Project.java
+++ src/main/java/memento/Project.java
@@ -5,7 +5,8 @@ import java.util.Date;
 
 public class Project {
     private String version;
-   private Date date;
+
+    private Date date;
 
     public void setVersionAndDate(String version){
         this.version = version;
